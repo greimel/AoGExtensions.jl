6ec6c5b0c0b3653c2ff14b952bb268c7025e8d33
Documenter.jl <documenter@juliadocs.github.io>
2025-10-06 14:25:33 +0000

    build based on 606fc09

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AoGExtensions","category":"page"},{"location":"#AoGExtensions","page":"Home","title":"AoGExtensions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AoGExtensions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AoGExtensions]","category":"page"}]
+[{"location":"#AoGExtensions","page":"Home","title":"AoGExtensions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AoGExtensions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
